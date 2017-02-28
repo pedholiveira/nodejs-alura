a02f80d90b0580f11da3b20f6d4ab79e7ee30be9
phoms <ped.holiveira@gmail.com>
2017-02-28 12:11:59 -0300

    Finalizada aula 3 - verbos http

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,9 +1,24 @@
 module.exports = function(app) {
-    app.get('/produtos', function (req, res) {
+    app.get('/produtos', function(req, res) {
         var connection = app.infra.connectionFactory(); 
         var dao = new app.infra.ProdutosDAO(connection);
         dao.lista(function(err,results){
             res.render('produtos/lista', {lista: results});
+        });
+        connection.end();
+    });
+
+    app.get('/produtos/form', function(req, res) {
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos/salva', function(req, res) {
+        var produto = req.body;
+
+        var connection = app.infra.connectionFactory();
+        var dao = new app.infra.ProdutosDAO(connection);
+        dao.salva(produto, function(err, results) {
+            res.redirect('/produtos');
         });
         connection.end();
     });
