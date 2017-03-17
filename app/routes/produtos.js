b2b714cbc5c38c005f61213e6be28954292d3df4
phoms <ped.holiveira@gmail.com>
2017-03-17 18:59:57 -0300

    Aula 6 finalizada

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -2,18 +2,41 @@ module.exports = function(app) {
     app.get('/produtos', function(req, res) {
         var connection = app.infra.connectionFactory(); 
         var dao = new app.infra.ProdutosDAO(connection);
-        dao.lista(function(err,results){
-            res.render('produtos/lista', {lista: results});
+        dao.lista(function(err,results) {
+            res.format({
+                html: function() {
+                    res.render('produtos/lista', { lista: results });
+                },
+                json: function() {
+                    res.json(results)
+                }
+            });
         });
         connection.end();
     });
 
     app.get('/produtos/form', function(req, res) {
-        res.render('produtos/form');
+        res.render('produtos/form', {produto: {}, erros: {}});
     });
 
-    app.post('/produtos/salva', function(req, res) {
+    app.post('/produtos', function(req, res) {
         var produto = req.body;
+
+        req.assert('titulo','Titulo é obrigatório').notEmpty();
+        req.assert('preco','Formato inválido').isFloat();
+
+        var erros = req.validationErrors();
+        if(erros){
+            res.format({
+                html: function(){
+                    res.status(400).render('produtos/form',{produto: produto, erros: erros});
+                },
+                json: function(){
+                    res.status(400).json(erros);
+                }
+            });
+            return;
+        }
 
         var connection = app.infra.connectionFactory();
         var dao = new app.infra.ProdutosDAO(connection);
